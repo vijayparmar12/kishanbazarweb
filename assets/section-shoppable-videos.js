c914e19fb9e903e9ad04acb95d778b7ea579e9ce
Vaibhavi Raut <rautvaibhavi2004@gmail.com>
2026-09-22 10:19:50 +0530

    fix(video): full-viewport backdrop blur for video reel modal

--- assets/section-shoppable-videos.js
+++ assets/section-shoppable-videos.js
@@ -94,6 +94,7 @@ class ShoppableVideosSection {
     this.modal.classList.add('is-open');
     this.modal.setAttribute('aria-hidden', 'false');
     document.body.classList.add('shoppable-modal-active');
+    document.documentElement.classList.add('shoppable-modal-active');
 
     // Play video with audio only when modal opens
     this.modalVideo.play().catch(() => {
@@ -111,6 +112,7 @@ class ShoppableVideosSection {
     this.modal.classList.remove('is-open');
     this.modal.setAttribute('aria-hidden', 'true');
     document.body.classList.remove('shoppable-modal-active');
+    document.documentElement.classList.remove('shoppable-modal-active');
   }
 
   bindModalEvents() {
